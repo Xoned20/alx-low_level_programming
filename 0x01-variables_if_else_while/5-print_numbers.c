27b5cee8ff412343b243d652060bc5b864ed90e9
Jephery Cyril <cyriljeph@gmail.com>
2022-09-09 10:40:31 -0700

    5-6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints all single digits in base 10 from 0.
+(*
+ *Return: 0 on success
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - prints single digits in base 10.
+ *
+ *Return: 0 on success
+ */
+int main(void)
+{
+	int a;
+
+	for (a ; a >= 0 ; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n')
+		return (0);
+}
