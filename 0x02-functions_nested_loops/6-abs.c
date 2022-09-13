3ef8e3abf7de6597fe09c918896ab020e6a58d0c
Jephery Cyril <cyriljeph@gmail.com>
2022-09-13 08:02:55 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.c"
+/**
+ * int _abs - computes the absolute value of an integer.
+ * @i: the integer to be converted
+ * Return: absoulte value of integer
+ */
+int _abs(int i)
+{
+	if (i < 0)
+		i = i * -1;
+	return (i);
+}
