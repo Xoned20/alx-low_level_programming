c22c5a8300805521d290532f1fb63cd8c11a80e2
Jephery Cyril <cyriljeph@gmail.com>
2022-09-13 12:13:13 -0700

    advanced

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the first 50 fibonacci number.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the sum of even-value terms
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+
+
+	
+       	return (0);
+}
