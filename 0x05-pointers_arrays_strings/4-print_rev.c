8ae7b2f5ad4e24b510fb047e05b050be7832d7fd
Jephery Cyril <cyriljeph@gmail.com>
2022-09-19 12:50:56 -0700

    3-5

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,16 +6,16 @@
  */
 void print_rev(char *s)
 {
-	int i;
+	int i = 0;
 
-	while (s[i] != '\0')
+	while (i >= 0)
 	{
+		if (s[i] == '\0')
+			break;
 		i++;
 	}
-	i--;
-	while (s[i] != '\0')
-	{
-		_putchar(s[i--]);
-	}
+
+	for (i--; i >= 0; i--)
+		_putchar(s[i]);
 	_putchar('\n');
 }
